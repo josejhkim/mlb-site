62e71ee0136958502a5f81deafe2e49a4ec5a138
Jose <barsamic03@gmail.com>
2024-12-13 21:01:49 +0900

    Add responsive layout to guide page

--- app/guide/page.tsx
+++ app/guide/page.tsx
@@ -4,16 +4,26 @@ const Page = () => {
   return (
     <>
       <div className="text-white flex flex-col items-center">
-        <h2 className="font-bold text-6xl mb-6">
+        <h2 className="font-bold 
+        text-5xl md:text-5xl lg:text-6xl 
+        mb-4 lg:mb-8">
           Guide
         </h2>
-        <h3 className="font-light text-xl">
+        <h3 className="font-light 
+        text-lg md:text-lg lg:text-xl">
           Start here if you&apos;re new to our product
         </h3>
       </div>
       <div className="flex flex-col items-center mt-16 pb-6">
-        <div className="text-white flex items-center border-2">
-          <iframe className='w-[700px] h-[380px]' src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=YtXmP2nVdFRP9fVw&amp;start=1" title="Demo" allow="accelerometerpted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <div className="text-white flex justify-center w-full items-center h-fit">
+          <iframe className='border-2 
+          w-[450px] h-[300px]
+          md:w-[600px] md:h-[400px]
+          lg:w-[840px] lg:h-[560px]
+          xl:w-[1000px] xl:h-[667px]
+          2xl:w-[1200px] 2xl:h-[900px]
+          '
+            src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=YtXmP2nVdFRP9fVw&amp;start=1" title="Demo" allow="accelerometerpted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
         <p className="text-white mt-6">
           Watch and follow our step-by-step video demo to get started
