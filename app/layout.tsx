c60e18487f2dee820c4df025c650633dd0dc02cf
Jose <barsamic03@gmail.com>
2024-12-13 18:36:02 +0900

    Place children inside inner div

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,14 +20,21 @@ export default function RootLayout({
           min-h-dvh
           bg-gradient-to-b from-[#1d0033] from-0% via-[#28102c] via-33% via-[#201624] via-66% to-[#1d0033] to-100%
           ${archivo.variable} 
-          bg-white
           font-[family-name:var(--font-archivo)]
-          mx-20
           overflow-x-hidden
           antialiased` }
       >
         <Navbar/>
-        {children}
+        <div         
+          className={`
+          min-h-dvh
+          bg-gradient-to-b from-[#1d0033] from-0% via-[#28102c] via-33% via-[#201624] via-66% to-[#1d0033] to-100%
+          ${archivo.variable} 
+          font-[family-name:var(--font-archivo)]
+          mx-20
+          antialiased` }>
+          {children}
+        </div>
       </body>
     </html>
   );
